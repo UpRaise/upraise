511bb3ac8004c28db727cc4ea5a751cb6ad7eb86
Dan Sweeney <dan@floatingboxes.com>
2014-10-05 14:26:55 -0500

    added smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,18 @@ function resizeDiv() {
   vph = $(window).height();
   $('.js-fill-viewport').css({'min-height': vph-20 + 'px'});
 }
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
