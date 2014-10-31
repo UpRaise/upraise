e33b8dce16e427aa92456c1f152ae1c347c05cee
Dan Sweeney <dan@floatingboxes.com>
2014-10-30 22:24:41 -0500

    moved events, links, and webinar data from YMLs to Google spreadsheets for easy client updates

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,24 @@ $(document).ready(function(){
     alignTop: true,
     overflowY: 'scroll'
   });
+
+
+  // Data from Google Spreadsheets for Events
+  $.get("https://data.brace.io/ss/qnFX7xbpdFBQP6RUKkPJXS", function(data) {
+    console.log(data);
+    injectEvents(data);
+  });
+
+  // Data from Google Spreadsheets for Blog Posts
+  $.get("https://data.brace.io/ss/bkcdrqo8YHAZZJ3p2QrKH5", function(data) {
+    injectLinks(data);
+  });
+
+  // Data from Google Spreadsheets for Webinars
+  $.get("https://data.brace.io/ss/RgnfrRYbXN7fKmfU5FjMCb", function(data) {
+    injectWebinars(data);
+  });
+
 });
 
 window.onresize = function(event) {
@@ -32,4 +50,40 @@ function resizeDiv() {
   vpw = $(window).width();
   vph = $(window).height();
   $('.js-fill-viewport').css({'min-height': vph-20 + 'px'});
+}
+
+function injectEvents(events) {
+  $.each(events["rows"], function(i, val) {
+    var eventHtml =  "<div class=\"event\">" +
+        "<a href=\"" + val.url + "\" class=\"event__link\">" +
+          "<div class=\"event__logo\"><div><img src=\"http://googledrive.com/host/0B8q2MFMBD9mFNmtMWHV5VFdnTEU/" + val.id + ".png\" alt=\"" + val.name + "\"></div></div>" +
+          "<div class=\"event__date\">" + val.date + "</div>" +
+        "</a>" +
+      "</div>";
+    $(".js-events").append(eventHtml);
+  });
+}
+
+
+function injectLinks(links) {
+  $.each(links["rows"], function(i, val) {
+    var linkHtml = "<div class=\"link\">" +
+        "<div class=\"link__bullet\"><div class=\"u-icon\"><img src=\"/img/icon-bullet.svg\"></div></div>" +
+        "<div class=\"link__date\">" + val.date + "</div>" +
+        "<div class=\"link__url\"><a href=\"" + val.url + "\">" + val.name + "</a></div>" +
+      "</div>";
+    $(".js-links").append(linkHtml);
+  });
+}
+
+function injectWebinars(webinars) {
+   $.each(webinars["rows"], function(i, val) {
+    var webinarHtml = "<div class=\"webinar\">" +
+        "<div class=\"webinar__content\">Join Our Upcoming Webinar “" + val.name + "” on " + val.date + " at " + val.time + ".</div>" +
+        "<div class=\"webinar__action\">" +
+          "<a href=\"" + val.url + "\" class=\"button button--reverse\">Sign Up Now</a>" +
+        "</div>" +
+      "</div>";
+    $(".js-webinars").append(webinarHtml);
+  });
 }
